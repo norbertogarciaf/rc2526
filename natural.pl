a365f72e44ebd55d004cd6f8d6578a356d2a884d
norbertogarciaf <norberto.garcia@alu.uhu.es>
2026-02-19 18:27:53 +0100

    Implement natural number logic in Prolog
    Define natural numbers using first-order logic.

---
+++ natural.pl
@@ -0,0 +1,35 @@
+
+/* LÓGICA DE PRIMER ORDEN */
+
+/*
+natural(Numero)
+Es cierto cuando Numero unifica con un número natural.
+
+natural(1).
+
+natural(n-1) -> natural(n)
+
+natural(N) :- N > 1, N2 is N-1, natural(N2).
+
+
+*/
+
+/*
+Principio de Inducción Matemático
+
+Sea S un conjunto ordenable.
+
+Sea n0 el elemento más pequeño de S.
+
+Quiero demostrar que la propiedad P se cumple
+para todo elemento del conjunto S.
+
+1. P es cierta para n0.
+2. Para todo elemento mayor que n0,
+si P(n-1) es cierto también lo será P(n).
+
+*/
+
+natural(1).
+
+natural(N) :- N > 1, N2 is N-1, natural(N2).
