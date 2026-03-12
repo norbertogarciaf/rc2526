9bfe283df0afd68be7a9dd28df29b8d1704374b8
norbertogarciaf <norberto.garcia@alu.uhu.es>
2026-03-12 09:03:06 +0100

    Refactor ordena_burbuja for clarity and comments

--- ordena_burbuja.pl
+++ ordena_burbuja.pl
@@ -7,10 +7,10 @@
 
 ordena_burbuja(Lista, Lista) :- ordenada(Lista).
 
-ordena_burbuja(Lista, RT):- append(Ini,[E1,E2|Fin], Lista),
-                            E1>E2,
-                            append(Ini, [E2,E1|Fin], R),
-                            ordena_burbuja(R, RT).
+ordena_burbuja(Lista, RT) :- append(Ini,[E1,E2|Fin], Lista), % coge un inicio, un par consecutivo de una lista (une dos listas)
+                             E1>E2,
+                             append(Ini, [E2,E1|Fin], R), % pone esa lista restante (R) con los elementos ahora ordenados
+                             ordena_burbuja(R, RT). % burbuja de lo que queda
 
 %-----------------------------------------------------
   % ordena(+Lista).
