09a6256a0f0227d64ac7bec06e8ddcdb03f22806
norbertogarciaf <norberto.garcia@alu.uhu.es>
2026-03-26 08:59:37 +0100

    Refactor permuta and selecciona_uno predicates

--- permuta.pl
+++ permuta.pl
@@ -1,24 +1,24 @@
 %--------------------------------------------------
-% selecciona_uno(+Lista, -Elem, -Resto)
-  % es cierto cuando Elem unifica con cualquier
-  % elemento de la lista Lista y Resto unifica
-  % con una lista que contiene los elementos de
-  % Lista, en el mismo orden menos el elemento Elem.
-%--------------------------------------------------
-
-selecciona_uno([Cab|Resto], Cab, Resto).
-
-selecciona_uno([Ca|Co], Elem, [Ca|Resto]) :- selecciona_uno(Co, Elem, Resto).
-    
-%--------------------------------------------------
 % permuta(Lista, ListaR).
-  % es cierto cuando ListaR unifica con una lista
-  % que contiene los elementos de Lista en orden
-  % distinto. Este predicado genera todas las
-  % listas posibles por backtraking.
+% es cierto cuando ListaR unifica con una lista
+% que contiene los elementos de Lista en orden
+% distinto. Este predicado genera todas las
+% listas posibles por backtraking.
 %--------------------------------------------------
 
 permuta([], []).
 
-permuta(L, [E|RP]):- selecciona_uno(L, E, R),
-					 permuta(R, RP).
+permuta(Lista, [Cab|Resto]) :- selecciona_uno(Cab, Lista, L),
+                               permuta(L, Resto).
+
+%--------------------------------------------------
+% selecciona_uno(+Lista, -Elem, -Resto)
+% es cierto cuando Elem unifica con cualquier
+% elemento de la lista Lista y Resto unifica
+% con una lista que contiene los elementos de
+% Lista, en el mismo orden menos el elemento Elem.
+%--------------------------------------------------
+
+selecciona_uno([Ca|R], Ca, R).
+
+selecciona_uno([Ca|Co], E, [Ca|R]) :- selecciona_uno(Co, E, R).
